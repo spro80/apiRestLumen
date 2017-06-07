72715a4fb60f4865ccdcb9ffcb6a1c18f1413426
David Diaz <david.diaz@zweicom.com>
2017-06-07 17:43:55 -0400

    modificacion en routesCategoria

--- app/Http/routes/routesCategoria.php
+++ app/Http/routes/routesCategoria.php
@@ -22,23 +22,21 @@ $app->group(['prefix' => 'api/sistema','namespace' => 'App\Http\Controllers'], f
     $app->get('categoria/getByTwoParameters/param1/{param1}/param2/{param2}','CategoriaController@getByTwoParameters');
     $app->get('categoria/getCountCategoria','CategoriaController@getCountCategoria');
     $app->get('categoria/getMaxIdCategoria','CategoriaController@getMaxIdCategoria');
-    $app->get('categoria/getMaxIdInsertarCategoria','CategoriaController@getMaxIdInsertarCategoria');
-    
+    $app->get('categoria/getMaxIdInsertarCategoria','CategoriaController@getMaxIdInsertarCategoria');    
     
     $app->get('categoria/subcategorias','CategoriaController@subcategorias');
     $app->get('categoria/prueba','CategoriaController@prueba');
 
     //URL TEST POSTMAN
-    //GET     http://localhost:8083/api/sistema/categoria	                       //EJEMPLO: ALL GET        - OK
-        //DELETE  http://localhost:8083/api/sistema/categoria/5                        //EJEMPLO: DELETE         - OK
-        //POST    http://localhost:8083/api/sistema/categoria                          //EJEMPLO: POST (CREATE)
-        //PUT     http://localhost:8083/api/sistema/categoria/4                        //EJEMPLO: PUT (UPDATE)   - OK
+    //GET     http://localhost:8083/api/sistema/categoria                          //EJEMPLO: ALL GET        - OK
+    //DELETE  http://localhost:8083/api/sistema/categoria/5                        //EJEMPLO: DELETE         - OK
+    //POST    http://localhost:8083/api/sistema/categoria                          //EJEMPLO: POST (CREATE)
+    //PUT     http://localhost:8083/api/sistema/categoria/4                        //EJEMPLO: PUT (UPDATE)   - OK
 
-
-    //http://localhost:8083/api/sistema/categoria/getByParameter/1                 //EJEMPLO: GET with 1 parameter-OK
+    //http://localhost:8083/api/sistema/categoria/getByParameter/1                            //EJEMPLO: GET with 1 parameter-OK
     //http://localhost:8083/api/sistema/categoria/getByTwoParameters/param1/ocho/param2/1     //EJEMPLO: GET with 2 parameter - OK
-    //GET     http://localhost:8083/api/sistema/categoria/getCountCategoria          //EJEMPLO: GET COUNT    - OK
-    //GET     http://localhost:8083/api/sistema/categoria/getMaxIdCategoria          //EJEMPLO: GET MAX ID   - OK
-    //GET     http://localhost:8083/api/sistema/categoria/getMaxIdInsertarCategoria  //EJEMPLO: GET MAX ID SIGUIENTE-OK
+    //GET     http://localhost:8083/api/sistema/categoria/getCountCategoria                   //EJEMPLO: GET COUNT    - OK
+    //GET     http://localhost:8083/api/sistema/categoria/getMaxIdCategoria                   //EJEMPLO: GET MAX ID   - OK
+    //GET     http://localhost:8083/api/sistema/categoria/getMaxIdInsertarCategoria           //EJEMPLO: GET MAX ID SIGUIENTE-OK
     
 });
